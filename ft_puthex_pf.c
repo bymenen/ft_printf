ab90f71fe124b709518588b30a707c6ab7df2b70
bymenen <148489583+bymenen@users.noreply.github.com>
2023-10-24 16:21:12 +0200

    Add files via upload

---
+++ ft_puthex_pf.c
@@ -0,0 +1,10 @@
+#include "ft_printf.h"
+
+void	ft_puthex_pf(unsigned int num, size_t *counter, char *base)
+{
+	char	*str;
+
+	str = ft_aux_pf(num, base);
+	ft_putstr_pf(str, counter);
+	free(str);
+}
---
+++ ft_putstr_pf.c
@@ -0,0 +1,12 @@
+#include "ft_printf.h"
+
+void	ft_putstr_pf(char *str, size_t *counter)
+{
+	if (!str)
+		str = "(null)";
+	while (*str)
+	{
+		ft_putchar_pf(*str, counter);
+		str++;
+	}
+}
